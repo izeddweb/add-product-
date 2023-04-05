19128e0df3eae297f09a1c4a953b1f93cb06139f
izedweb <abzedd61@gmail.com>
2023-04-05 17:32:19 +0100

    finiching footer branch

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //     set variable
 const body = document.querySelector("body");
 
-//     Star Header  1 -1
+//     ******************************* Star Header  1 -1
 
 //  create header elements
 const header = document.createElement("header");
@@ -49,7 +49,7 @@ div.appendChild(textLogo);
 
 // styling elements   -1   body
 body.style.cssText =
-  "background-color :#eee; height:60px;width:100vw;display:flex;justify-content:space-evenly;align-item:center;height:1500px;";
+  "background-color :#eee; height:100%;width:100vw;";
 // styling elements   -1   header
 header.style.cssText =
   "background-color :#26ff00; height:60px;width:100vw;display:flex;justify-content:space-evenly;align-item:center;position:fixed; ";
@@ -66,58 +66,77 @@ nav.style.cssText =
 ul.style.cssText = "height:100%;width:100%;display:flex;list-style-type:none;";
 
 //  end header
-//  star main section 2
+
+// ******************************************** star main section 2
 
 // create element main section
 
 const main = document.createElement("main");
 const mainContent = document.createElement("div");
 
-
 // styling element main
-main.style.cssText = " width:100%;height:auto;"
-mainContent.style.cssText =" width:80%;height:auto;margin:90px auto;display:flex;flex-wrap: wrap;justify-content :center;"
+main.style.cssText = " width:100%;height:auto;";
+mainContent.style.cssText =
+  " width:80%;height:auto;margin:0 auto;display:flex;flex-wrap: wrap;justify-content :center; padding-top: 105px;";
 
 // create function
 function addingElementsMain() {
-  
-  for(let i =1; i<= 15; i++ ) {
+  for (let i = 1; i <= 15; i++) {
     const productsCard = document.createElement("div");
     // adding text node to product
-    const productName = document.createTextNode(`Product Name:`)
+    const productName = document.createTextNode(`Product Name:`);
     const products = document.createElement("span");
     // styling productsCard
-   
-    productsCard.addEventListener("mousemove" ,hoverEffects)
-    function hoverEffects(){
-      productsCard.style.backgroundColor ="red"
-      productsCard.style.transition=".7s"
+
+    productsCard.addEventListener("mousemove", hoverEffects);
+    function hoverEffects() {
+      productsCard.style.backgroundColor = "red";
+      productsCard.style.transition = ".7s";
     }
-   
-    hoverEffects()
 
-    
+    hoverEffects();
+    productsCard.style.cssText =
+      " background-color:#e8e8e8; width:320px;height:320px;padding :10px;margin :10px auto;box-shadow:0 0 10px #00000069;display:flex;align-items:center;justify-content:center;flex-direction: column;";
 
-
-    productsCard.style.cssText =" background-color:#e8e8e8; width:320px;height:320px;padding :10px;margin :10px auto;box-shadow:0 0 10px #00000069;display:flex;align-items:center;justify-content:center;flex-direction: column;"
-
-    products.style.cssText="display:block;font-size:50px; text-align:center;"
-    products.prepend(`${i}`)
-    productsCard.appendChild(products)
-    productsCard.appendChild(productName)
-    productsCard.className= "products-card"
-    mainContent.appendChild(productsCard)
+    products.style.cssText = "display:block;font-size:50px; text-align:center;";
+    products.prepend(`${i}`);
+    productsCard.appendChild(products);
+    productsCard.appendChild(productName);
+    productsCard.className = "products-card";
+    mainContent.appendChild(productsCard);
   }
-  main.appendChild(mainContent)
+  main.appendChild(mainContent);
   body.appendChild(main);
 }
 
 addingElementsMain();
 
 // adding classes to elements main
-mainContent.className= "main-content"
+mainContent.className = "main-content";
 main.classList.add("main");
 
-
-
 //  end main section
+//  ************************************ star footer section
+
+// create element footer and textNode
+const footer = document.createElement("footer");
+const footerPara = document.createElement("p");
+const textPara = document.createTextNode(" Copyright 2023 --zed .D");
+// styling elements
+
+footer.style.cssText =
+  "background-color :#26ff00; height:60px;width:100vw;display:flex;justify-content:center; align-items:center;";
+footerPara.style.cssText ="background-color :#26ff00; height:60px;width:100vw;display:flex;justify-content:center; align-items:center;";
+
+// assign class to footer elements
+footer.className = "footer";
+footerPara.className = "footer-para";
+
+// appendelement to body
+function addingFooter() {
+  footerPara.appendChild(textPara)
+  footer.appendChild(footerPara);
+  body.appendChild(footer);
+}
+
+addingFooter();
